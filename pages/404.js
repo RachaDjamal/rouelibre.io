38adffd300cb9c351eee6795a092a3eb2fa0ea77
RachaDjamal <sacha.mallet1@gmail.com>
2022-01-06 15:44:54 +0100

    Fix typo

--- pages/404.js
+++ pages/404.js
@@ -2,11 +2,14 @@ import React from "react"
 import Link from "next/link"
 
 export default function Custom404() {
+
+    const text = "Oops, il semble que cette page n'existe pas"
+
     return (
         <>
             <div className="erreur">
                 <h1>🤯</h1>
-                <h2>Oops, il semble que cette page n'existe pas</h2>
+                <h2>{text}</h2>
                 <Link href="/"><a  className="">Revenir à la homepage</a></Link>
             </div>
         </>
